ba80550d0e67a9536784ac737f4d93afb57f9b80
salman <salman.khalid@prettylittlething.com>
2019-12-12 13:17:22 +0500

    changes 2

--- lambda/test/index.js
+++ lambda/test/index.js
@@ -1,29 +1,9 @@
 const AWS = require('aws-sdk');
 const documentClient = new AWS.DynamoDB.DocumentClient();
 
-const getlist = function(limit){
-	var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
-	return ddb.listTables({Limit: limit}).promise()
-}
-
 exports.handler = async (event) => {	  
-	try {
-		let list = await getlist(10);
-		return  {
-			statusCode: 200,
-			headers: {
-			  "Access-Control-Allow-Origin": "*"
-			},
-			body: JSON.stringify(list.TableNames)
-		};
-	}
-	catch(e){
-		return  {
-			statusCode: 400,
-			headers: {
-				"Access-Control-Allow-Origin": "*"
-			},
-			body: err.code
-		};
-	}
+	return  {
+		statusCode: 200,		
+		body: "this is a new route"
+	};	
 };
